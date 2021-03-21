85472166e821ac4855aaa95bdc7387bab1d475f6
高博 <ip13772600507@gmail.com>
2021-03-21 23:19:32 +0800

    商品详情&评价信息
    Signed-off-by: 高博 <ip13772600507@gmail.com>

--- foodie-dev-service/src/main/java/com/example/service/ItemsService.java
+++ foodie-dev-service/src/main/java/com/example/service/ItemsService.java
@@ -2,8 +2,34 @@ package com.example.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.pojo.Items;
+import com.example.pojo.ItemsImg;
+import com.example.pojo.ItemsParam;
+import com.example.pojo.ItemsSpec;
+import com.example.utils.PagedGridResult;
+import com.example.vo.CommentLevelCountsVO;
+import com.example.vo.ShopcartVO;
+import java.util.List;
 
 public interface ItemsService extends IService<Items>{
 
+  /**
+   * 根据商品ID查询商品
+   * @param itemId
+   * @return
+   */
+  Items queryItemById(String itemId);
 
+  List<ItemsImg> queryItemImgList(String itemId);
+
+  List<ItemsSpec> queryItemSpecList(String itemId);
+
+  ItemsParam queryItemParam(String itemId);
+
+  CommentLevelCountsVO queryCommentCounts(String itemId);
+
+  PagedGridResult searchItems(String keywords, String sort, Integer page, Integer pageSize);
+
+  PagedGridResult queryPagedComments(String itemId, Integer level, Integer page, Integer pageSize);
+
+  List<ShopcartVO> queryItemsBySpecIds(String itemSpecIds);
 }
--- foodie-dev-service/src/main/java/com/example/service/impl/ItemsServiceImpl.java
+++ foodie-dev-service/src/main/java/com/example/service/impl/ItemsServiceImpl.java
@@ -1,12 +1,99 @@
 package com.example.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.enums.CommentLevel;
+import com.example.mapper.ItemsCommentsMapper;
+import com.example.mapper.ItemsImgMapper;
 import com.example.mapper.ItemsMapper;
+import com.example.mapper.ItemsParamMapper;
+import com.example.mapper.ItemsSpecMapper;
 import com.example.pojo.Items;
+import com.example.pojo.ItemsComments;
+import com.example.pojo.ItemsImg;
+import com.example.pojo.ItemsParam;
+import com.example.pojo.ItemsSpec;
 import com.example.service.ItemsService;
+import com.example.utils.PagedGridResult;
+import com.example.vo.CommentLevelCountsVO;
+import com.example.vo.ShopcartVO;
+import java.util.List;
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 @Service
 public class ItemsServiceImpl extends ServiceImpl<ItemsMapper, Items> implements ItemsService {
 
+  @Autowired ItemsMapper itemsMapper;
+  @Autowired ItemsImgMapper itemsImgMapper;
+  @Autowired ItemsSpecMapper itemsSpecMapper;
+  @Autowired ItemsParamMapper itemsParamMapper;
+  @Autowired ItemsCommentsMapper itemsCommentsMapper;
+
+  /**
+   * @param itemId
+   * @return
+   */
+  @Override
+  public Items queryItemById(String itemId) {
+    return itemsMapper.selectById(itemId);
+  }
+
+  @Override
+  public List<ItemsImg> queryItemImgList(String itemId) {
+
+    return itemsImgMapper.selectList(
+        new LambdaQueryWrapper<ItemsImg>().eq(ItemsImg::getItemId, itemId));
+  }
+
+  @Override
+  public List<ItemsSpec> queryItemSpecList(String itemId) {
+
+    return itemsSpecMapper.selectList(
+        new LambdaQueryWrapper<ItemsSpec>().eq(ItemsSpec::getItemId, itemId));
+  }
+
+  @Override
+  public ItemsParam queryItemParam(String itemId) {
+    return itemsParamMapper.selectOne(
+        new LambdaQueryWrapper<ItemsParam>().eq(ItemsParam::getItemId, itemId));
+  }
+
+  @Override
+  public CommentLevelCountsVO queryCommentCounts(String itemId) {
+    Integer goodC = getCommentCounts(itemId, CommentLevel.GOOD.type);
+    Integer normalC = getCommentCounts(itemId, CommentLevel.NORMAL.type);
+    Integer badC = getCommentCounts(itemId, CommentLevel.BAD.type);
+    return new CommentLevelCountsVO(){{
+      setBadCounts(badC);
+      setNormalCounts(normalC);
+      setGoodCounts(goodC);
+      setTotalCounts(goodC + normalC + badC);
+    }};
+  }
+
+  private Integer getCommentCounts(String itemId, Integer level) {
+
+    return itemsCommentsMapper.selectCount(
+        new LambdaQueryWrapper<ItemsComments>()
+            .eq(ItemsComments::getItemId, itemId)
+            .eq(Objects.nonNull(level), ItemsComments::getCommentLevel, level));
+  }
+
+  @Override
+  public PagedGridResult searchItems(String keywords, String sort, Integer page, Integer pageSize) {
+    return null;
+  }
+
+  @Override
+  public PagedGridResult queryPagedComments(
+      String itemId, Integer level, Integer page, Integer pageSize) {
+    return null;
+  }
+
+  @Override
+  public List<ShopcartVO> queryItemsBySpecIds(String itemSpecIds) {
+    return null;
+  }
 }
