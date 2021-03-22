6487b46fe8ed97f60bdf62506a09c3a6d697cab6
CT-Choochoo <ip13772600507@gmail.com>
2021-03-22 22:03:51 +0800

    提交

--- foodie-dev-api/src/main/java/com/example/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/example/controller/IndexController.java
@@ -11,8 +11,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import java.util.List;
 import javax.validation.constraints.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- foodie-dev-api/src/main/java/com/example/controller/UserController.java
+++ foodie-dev-api/src/main/java/com/example/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.pojo.Users;
 import com.example.service.UsersService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +18,6 @@ public class UserController {
   @GetMapping("/list")
   public List<Users> list(@RequestParam(name = "name", required = false) String name) {
     return usersService.list(name);
-  }
-  @GetMapping("/get")
-  public Users get(@RequestParam("id") String id) {
-    return usersService.getById(id);
-  }
-
-  @PutMapping("/update")
-  public void update(@RequestBody Users users) {
-    usersService.updateById(users);
-  }
-
-  @DeleteMapping("/delete")
-  public void delete(@RequestBody String id) {
-    usersService.removeById(id);
   }
 
 }
